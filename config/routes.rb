5cdab8f2c66710644462523528dfcc266e025c68
DiegoGomezCuadross <digoc_123hotmail.comm>
2023-07-19 16:04:10 -0500

    add: properties show, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
 resources :properties
 
+# Users
+resources :users, only: :create
+get "/profile", to: "users#show"
+patch "/profile", to: "users#update"
+
 #sessions
  post "/login" => "sessions#create"
  delete "/logout" => "sessions#destroy"

13e1fe3fc8e8f229286099ceb51a7dc21a230461
pdroariasvilela <pdroariasvilela@gmail.com>
2023-07-22 11:04:31 -0500

    add :active / property controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -51,6 +51,6 @@ class PropertiesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def property_params
-    params.permit(:operation_type, :address, :price, :montly_rent, :maintanance, :property_type, :bedrooms, :bathrooms, :area, :pets, :about, :photo, :user_id)
+    params.permit(:operation_type, :address, :price, :montly_rent, :maintanance, :property_type, :bedrooms, :bathrooms, :area, :pets, :about, :photo, :user_id, :active)
   end
 end
